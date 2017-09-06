c2ab2c74c7ce78fd89a6ae557b638658ecb8b554
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2017-09-06 20:37:44 +0800

    feat: langSwitch with flag

--- src/components/langSwitch.js
+++ src/components/langSwitch.js
@@ -2,43 +2,61 @@ import React, { Component } from 'react'
 import { Dropdown, Menu, Button } from 'antd'
 
 const Item = Menu.Item
-const LANGS = {
-  zh_cn: 'ENGLISH',
-  en_us: 'CHINESE',
+
+const ICon = ({ className, text }) => {
+  return (
+    <div>
+      <svg className="icon" aria-hidden="true">
+        <use xlinkHref={className} />
+      </svg>
+      <span style={{ marginLeft: '0.5em' }}>{text}</span>
+    </div>
+
+  )
+}
+
+const LANGS = [
+  'en_us',
+  'zh_cn',
+]
+
+const LANGS_TEXT = {
+  zh_cn: 'CHINESE',
+  en_us: 'ENGLISH',
+}
+
+const LANGS_ICON = {
+  zh_cn: '#icon-china',
+  en_us: '#icon-english',
 }
 
 class Language extends Component {
+
   onSelect = ({ key }) => {
-    const { changeLanguageTo } = this.props
-    changeLanguageTo(key)
+    this.props.changeLanguageTo(key)
   }
 
   render() {
     const { locale } = this.props
     const menu = (
       <Menu onSelect={this.onSelect} style={{ textAlign: 'center' }}>
-        {/* item */}
-        <Item key="en_us">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-english" />
-          </svg>
-          <span style={{ marginLeft: '0.5em' }}>ENGLISH</span>
-        </Item>
-
-        {/* item */}
-        <Item key="zh_cn">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-china" />
-          </svg>
-          <span style={{ marginLeft: '0.5em' }}>CHINESE</span>
-        </Item>
+        {
+          LANGS.map(v => {
+            console.log(v)
+            return (
+              <Item key={v}>
+                <ICon className={LANGS_ICON[v]} text={LANGS_TEXT[v]} />
+              </Item>
+            )
+          })
+        }
       </Menu>
     )
 
     return (
       <Dropdown overlay={menu} placement="bottomCenter">
         <Button>
-          {LANGS[locale]}
+          <ICon className={LANGS_ICON[locale]} text={LANGS_TEXT[locale]} />
         </Button>
       </Dropdown>
     )
